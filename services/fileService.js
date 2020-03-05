c7800680bfb90520daaac2bf59ebdab69a0f28d6
anansantos <anansantos@brasilseg.com.br>
2020-03-05 11:21:45 -0300

    Adding the functionality of setting a root dir to create components.

---
+++ services/fileService.js
@@ -0,0 +1,42 @@
+const { resolver } = require("../helpers/componentPathResolver");
+const funcComponent = require("../templates/funcComponent");
+const classComponent = require("../templates/classComponent");
+const test = require("../templates/test");
+const fs = require("fs");
+const { join } = require("path");
+
+module.exports = {
+  componentCreate: (type, componentDir, name, rootDir, userName) => {
+    const componentPath = resolver(rootDir, componentDir, name, ".js");
+    const stylePath = resolver(rootDir, componentDir, name, ".scss");
+    const createdAt = new Date().toLocaleDateString("pt-br");
+    const content =
+      type === "stf"
+        ? classComponent.template(`${name}.scss`, userName, createdAt)
+        : funcComponent.template(`${name}.scss`, userName, createdAt);
+
+    fs.writeFileSync(componentPath, content);
+    fs.writeFileSync(stylePath, ".container {}");
+  },
+  testCreate: (testDir, testName) => {
+    const testPath = join(testDir, "", `${testName}.spec.js`);
+    const content = test.template(testName);
+    fs.writeFileSync(testPath, content);
+  },
+  createIfNoExist: (path, shouldThrowError, errorMessage) => {
+    if (!fs.existsSync(path)) {
+      fs.mkdirSync(path, { recursive: true });
+    } else {
+      if (shouldThrowError) throw new Error(errorMessage);
+    }
+  },
+
+  createRootPath: path => {
+    fs.writeFileSync("rootPath.txt", path);
+  },
+
+  checkRootPathFile: () => {
+    if (!fs.existsSync("rootPath.txt")) return false;
+    return fs.readFileSync("rootPath.txt", "utf8");
+  }
+};
